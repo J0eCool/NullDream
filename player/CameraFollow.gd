0692c190e0c69728f94dbb359dcf799f2e1c1546
J0eCool <count.j0ecool@gmail.com>
2023-11-29 21:56:20 -0800

    camera calculates its own bounds, tilemap set up with Terrains

--- player/CameraFollow.gd
+++ player/CameraFollow.gd
@@ -1,8 +1,19 @@
 extends Camera2D
 
 @export var target: Node2D
+@export var tiles: TileMap
+
+
+func _ready():
+	# Automatically set the Limits of the camera to be within the given tilemap
+	# Assumes the tilemap is convex
+	var size = tiles.tile_set.tile_size
+	var used = tiles.get_used_rect()
+	limit_left = tiles.position.x + size.x * used.position.x
+	limit_right = tiles.position.x + size.x * used.end.x
+	limit_top = tiles.position.y + size.y * used.position.y
+	limit_bottom = tiles.position.y + size.y * used.end.y
 
 
 func _physics_process(_dt):
-	# hey what if we had the camera kept within the bounds of a target tilemap
 	position = target.position
