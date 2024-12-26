0306749fc54d28c34c1b4604f046b2d5c08b455f
J0eCool <count.j0ecool@gmail.com>
2024-12-26 04:05:03 -0800

    Theming. fakeass damage calculation

--- BlackjackRPG/blackjack_rpg.gd
+++ BlackjackRPG/blackjack_rpg.gd
@@ -7,26 +7,26 @@ func draw_card() -> void:
 	var card: Card = card_scene.instantiate()
 	card.suit = randi()%4
 	card.val = randi()%13
-	card.position = Vector2(100*$Cards.get_child_count(), 0)
-	$Cards.add_child(card)
+	card.position = Vector2(100*$Player/Cards.get_child_count(), 0)
+	$Player/Cards.add_child(card)
 
 	var hand = hand_value()
-	$HandValue.text = str(hand)
+	$Player/HandValue.text = str(hand)
 	var color = Color.WHITE
 	if hand == 21:
 		color = Color.YELLOW
 	elif hand > 21:
 		color = Color.RED
-	$HandValue.label_settings.font_color = color
+	$Player/HandValue.label_settings.font_color = color
 
 
 func hand_value() -> int:
 	var value = 0
-	for card: Card in $Cards.get_children():
+	for card: Card in $Player/Cards.get_children():
 		value += card.get_value()
 	# second pass if Aces are causing us to go over 21
 	# (aces can be either 1 or 11)
-	for card: Card in $Cards.get_children():
+	for card: Card in $Player/Cards.get_children():
 		if value > 21 and card.val == Card.CardVal.Ace:
 			value -= 10
 	return value
@@ -44,14 +44,22 @@ func _process(_dt: float) -> void:
 
 func new_hand() -> void:
 	# clear hand
-	for child in $Cards.get_children():
-		$Cards.remove_child(child)
+	for child in $Player/Cards.get_children():
+		$Player/Cards.remove_child(child)
 		child.queue_free()
 	for i in range(2):
 		draw_card()
 
 
 func _on_new_hand_pressed() -> void:
+	var damage = hand_value() - 10
+	if damage == 11:
+		damage = 15
+	elif damage > 11:
+		damage = -10
+	var word = "dealt" if damage > 0 else "took"
+	$LogLabel.text = "You {0} {1} damage".format([word, abs(damage)])
+
 	new_hand()
 
 
